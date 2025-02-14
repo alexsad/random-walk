8db9643bb81166549c854cba6f5c0f15262eb55c
alexandre.araujo <alexandre.araujo@mv.com.br>
2025-02-14 17:02:26 -0300

    switch cases

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './components/avatar-grid';
 // <avatar-grid></avatar-grid>
-import './example-order';
-import './example-order-v2';
+// import './example-order';
+// import './example-order-v2';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div style="padding: 5rem">    
+  <div style="padding: 5rem">
+  <avatar-grid></avatar-grid>
   </div>
 `; 
